3b0c4906f5aaae947c663ea9d99c66290bafc608
nicole-fer <nicolefernandes@alu.ufc.br>
2023-03-30 09:53:18 -0300

    Aulas de 01 a 07 do curso de node.js

---
+++ Aula06-07/app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+
+
+//definindo rotas
+
+app.get("/", function(req,res){
+    res.send("Hello World!")
+})
+
+app.get("/home", function(req,res){
+    res.send("Rota Home")
+})
+
+app.get("/about", function(req,res){
+    res.send("Rota About")
+})
+
+//passando paramentros na rota 
+app.get("/ola:nome", function(req,res){
+    res.send("<h1>Olá " + req.params.nome + "</h1>")
+})
+
+ 
+//localhost:8081
+app.listen(8081, function(){
+    console.log("Servidor rodando")
+}) //deixa o servidor rodando e deve ser a última do código
+
