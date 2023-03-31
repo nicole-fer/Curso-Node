d6ed9cdbcd88d417b50033f1ba2be526c911543d
nicole-fer <nicolefernandes@alu.ufc.br>
2023-03-31 08:50:11 -0300

    Principais comando em Mysql

---
+++ comandos.sql
@@ -0,0 +1,28 @@
+/* Criar uma tabela */
+CREATE TABLE  usuarios( 
+    nome VARCHAR(50),
+    email VARCHAR(100),
+    idade INT
+);
+
+/* Comado pra rodar no cmd -> mysql -h localhost -u root -p */
+/* Comando ara acessar um banco de dados -> USE sistemadecadastro */
+/* Comando para mostrar as tabelas -> SHOW TABLES; */
+/* Comando para ver uma tabelas -> DESCRIBE usuarios; */
+/* Para listar dados de uma tabela -> SELECT * FROM usuarios; */
+/* Para fazer consultas . Ex.: SELECT * FROM usuarios WHERE idade = 23 */
+
+
+/* Inserir dados na tabela */
+INSERT INTO usuarios(nome, email, idade) VALUES (
+    "Nicole",
+    "nicolefernandesfm@gmail.com",
+    23
+);
+
+
+/* Deleta um registro no banco de dados */
+DELETE FROM usuarios WHERE nome = "Jose";
+
+/* Atualizando registros no banco */
+UPDATE usuarios SET nome = "Ana", email = "Ana@gmail.com" WHERE nome = "Maria";
