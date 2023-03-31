4456e54d9f53d19611155b8265f61767457dd300
nicole-fer <nicolefernandes@alu.ufc.br>
2023-03-31 10:01:54 -0300

    Reformulando projeto para Handlebars

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express")
 const app = express()
 const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
 
 // Config
     //Template Engine
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
- 
+    // Conexão com o banco de dados mysql
+    const sequelize = new Sequelize('sistemadecadastro', 'root', 'nicole', {
+        host: "localhost",
+        dialect: "mysql"
+    })
+    
+
+
 //localhost:8081
 app.listen(8081, function(){
     console.log("Servidor rodando")
