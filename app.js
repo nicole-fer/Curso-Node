32db0e258705985f61eb4da6bd8b5d1adff6cd61
nicole-fer <nicolefernandes@alu.ufc.br>
2023-04-04 13:44:27 -0300

    Adicionando e configurando o Body Parser

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require("express")
 const app = express()
-const handlebars = require('express-handlebars')
+const { engine } = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize')
 
 // Config
     //Template Engine
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.engine('handlebars', engine())
         app.set('view engine', 'handlebars')
+        app.set('views', "./views")
+
+    // BodyParser
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
+        
     // Conexão com o banco de dados mysql
-    const sequelize = new Sequelize('sistemadecadastro', 'root', 'nicole', {
-        host: "localhost",
-        dialect: "mysql"
+        const sequelize = new Sequelize('sistemadecadastro', 'root', 'nicole', {
+            host: "localhost",
+            dialect: "mysql"
+        })
+
+//Rotas
+    app.get('/cad', function(req, res){
+        res.render('formulario')
+    })
+    // Rota post
+    app.post('/send', function( req, res) {
+        res.send('Texto: ' + req.body.titulo + " Conteudo: " + req.body.conteudo)
     })
     
 
