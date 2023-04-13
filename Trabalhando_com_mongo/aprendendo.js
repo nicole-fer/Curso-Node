e9bf4e57b0eeec72ea50aa2b9e1139e0710c6836
nicole-fer <nicolefernandes@alu.ufc.br>
2023-04-13 09:33:27 -0300

    Inserindo novo usuario no banco com mongoose

--- Trabalhando_com_mongo/aprendendo.js
+++ Trabalhando_com_mongo/aprendendo.js
@@ -3,9 +3,52 @@
 const mongoose = require("mongoose")
 
 mongoose.connect("mongodb://127.0.0.1/umbancoqualquer", {
-    useMongoClient: true
 }).then(() => {
     console.log("Conectado")
 }).catch((err) => {
     console.log("Houve um erro ao se conectar com o mongoDB" + err)
+})
+
+
+//Model - Usuários
+
+const UsuariosSchema = mongoose.Schema({
+
+    nome: {
+        type: String,
+        require: true,
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String
+    }
+})
+
+// Collection
+mongoose.model('usuarios', UsuariosSchema)
+
+// inserindo novo usuário no banco 
+const novoUsuario = mongoose.model('usuarios')
+
+new novoUsuario({
+    nome: "Nicole",
+    sobrenome: "Fernandes",
+    email: "nicolefernanes@gmail.com",
+    idade: 23,
+    pais: "Brasil"
+}).save().then(() => {
+    console.log("Sucesso")
+}).catch((err) => {
+    console.log("Erro" + err)
 })
